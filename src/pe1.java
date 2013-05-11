346b610b9ff67c8a046e747d372262f28517859b
mahbuborama <mahbuborama@gmail.com>
2013-05-10 22:47:40 -0400

    Added Problem #2

--- src/pe1.java
+++ src/pe1.java
@@ -1,4 +1,4 @@
-/* 
+/* PROBLEM 1:
  * If we list all the natural numbers below 10 that are multiples of 3 or
  * 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  * 
@@ -9,10 +9,16 @@ public class pe1 {
 	
 	public static void main(String[] args) {
 		int the_sum = 0;
+		
+		/* Loop through all numbers below 1000 and use the modulus operator to see if
+		 * 'i' is a multiple of 3 or 5, i.e. if it has a remainder of 0 when divided 
+		 * by 3 or 5. If 'i' is a multiple of 3 or 5, add it to our total sum.
+		 */
 		for (int i=0; i<1000; i++) {
 			if ((i % 3 == 0) | (i % 5 == 0))
 				the_sum = the_sum + i;
 		}
+		
 		System.out.println(the_sum);
 	}
 
